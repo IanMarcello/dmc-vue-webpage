97ab00f724ec5d16f1015de43d79c6e976dbfb2f
Ian Marcello Jada <ian.marcello.jada19@gmail.com>
2023-05-31 12:36:17 +0300

    Add custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,17 @@ module.exports = {
     extend: {
       fontFamily: {
         dmc: ["DMC5Font"],
+        'montserratalternates-thin': ["MontserratAlternates-Thin"],
+        'montserratalternates-extralight': ["MontserratAlternates-ExtraLight"],
+        'montserratalternates-light': ["MontserratAlternates-Light"],
+        'montserratalternates-regular': ["MontserratAlternates-Regular"],
+        'montserratalternates-medium': ["MontserratAlternates-Medium"],
+        'montserratalternates-semibold': ["MontserratAlternates-SemiBold"],
+        'montserratalternates-bold': ["MontserratAlternates-Bold"],
+        'montserratalternates-extrabold': ["MontserratAlternates-ExtraBold"],
+        'montserratalternates-black': ["MontserratAlternates-Black"],
       },
     },
   },
   plugins: [],
 }
-
