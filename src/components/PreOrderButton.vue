2c43438354a7f6d25b810b9d4858db6dea7a4e60
Ian Marcello Jada <ian.marcello.jada19@gmail.com>
2023-06-01 17:03:17 +0300

    Home view first section

--- src/components/PreOrderButton.vue
+++ src/components/PreOrderButton.vue
@@ -1,19 +1,48 @@
 <template>
   <div>
-    <button class="btn-gradient relative text-xl px-7 py-2">PRE-ORDER</button>
+    <button class="btn-gradient relative btn from-left hover:tracking-wider text-xl px-7 py-2 z-10">PRE-ORDER</button>
   </div>
 </template>
 
 <style>
 .btn-gradient {
-  background: hsla(184, 41%, 45%, 1);
+  background: #449CA2;
 
-  background: linear-gradient(315deg, hsla(184, 41%, 45%, 1) 20%, hsla(4, 78%, 55%, 1) 70%);
+  background: linear-gradient(315deg, #449CA2 20%, #E63F33 70%);
 
-  background: -moz-linear-gradient(315deg, hsla(184, 41%, 45%, 1) 20%, hsla(4, 78%, 55%, 1) 70%);
+  background: -moz-linear-gradient(315deg, #449CA2 20%, #E63F33 70%);
 
-  background: -webkit-linear-gradient(315deg, hsla(184, 41%, 45%, 1) 20%, hsla(4, 78%, 55%, 1) 70%);
+  background: -webkit-linear-gradient(315deg, #449CA2 20%, #E63F33 70%);
 
   filter: progid: DXImageTransform.Microsoft.gradient( startColorstr="#449BA2", endColorstr="#E63D32", GradientType=1 );
+}
+
+.btn {		
+	transition: all 500ms cubic-bezier(0.77, 0, 0.175, 1);
+}
+
+.btn:before, .btn:after {
+	content: '';
+	position: absolute;	
+	transition: inherit;
+	z-index: -1;
+}
+
+.btn:hover:after {
+	background: #449CA2;
+	transition-delay: .35s;
+}
+
+.from-left:before,
+.from-left:after {
+  top: 0;
+  left: 0;
+  width: 0;
+  height: 100%;
+}
+
+.from-left:hover:before,
+.from-left:hover:after {
+  width: 100%;
 }
 </style>
---
+++ src/views/Nav/ContactView.vue
@@ -0,0 +1,31 @@
+<script setup>
+import IcBaselineDiscord from '~icons/ic/baseline-discord'
+import MdiGithub from '~icons/mdi/github'
+import MdiTwitter from '~icons/mdi/twitter'
+import MdiLinkedin from '~icons/mdi/linkedin'
+</script>
+
+<template>
+  <div class="flex relative gap-x-5 -top-[85px] -left-5 py-2 px-4 bg-[#16191f] w-fit">
+    <div>
+      <a href="#" class="transition-colors hover:text-red-600" target="_blank"
+        ><MdiLinkedin class="w-6 h-6"
+      /></a>
+    </div>
+    <div>
+      <a href="#" class="transition-colors hover:text-red-600" target="_blank"
+        ><IcBaselineDiscord class="w-6 h-6"
+      /></a>
+    </div>
+    <div>
+      <a href="#" class="transition-colors hover:text-red-600" target="_blank"
+        ><MdiGithub class="w-6 h-6"
+      /></a>
+    </div>
+    <div>
+      <a href="#" class="transition-colors hover:text-red-600" target="_blank"
+        ><MdiTwitter class="w-6 h-6"
+      /></a>
+    </div>
+  </div>
+</template>
