d638546f67fd37e83979bffe886eec3c8fb4ec8f
Ian Marcello Jada <ian.marcello.jada19@gmail.com>
2023-06-10 12:17:14 +0300

    Update tailwind.config.js
    Added custom breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -43,7 +43,8 @@ module.exports = {
       // 'lg': '1024px',
       // 'xl': '1280px',
       // '2xl': '1536px',
-      '3xl': '1600px'
+      '3xl': '1600px',
+      '4xl': '2000px'
     }
   },
   plugins: []
