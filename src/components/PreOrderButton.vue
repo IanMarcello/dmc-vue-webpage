c10624918f106ccc0e801c64e8999fb64ee3af89
Ian Marcello Jada <ian.marcello.jada19@gmail.com>
2023-06-30 14:54:30 +0300

    Updated components
    Minor changes

--- src/components/PreOrderButton.vue
+++ src/components/PreOrderButton.vue
@@ -1,38 +1,43 @@
 <template>
   <div>
     <a href="#">
-      <button class="btn-gradient relative btn from-left hover:tracking-wider text-xl px-8 py-2 z-10">PRE-ORDER</button>
+      <button
+        class="relative z-10 px-8 py-2 text-xl btn-gradient btn from-left hover:tracking-wider"
+      >
+        PRE-ORDER
+      </button>
     </a>
   </div>
 </template>
 
 <style>
 .btn-gradient {
-  background: #449CA2;
+  background: #449ca2;
 
-  background: linear-gradient(315deg, #449CA2 20%, #E63F33 70%);
+  background: linear-gradient(315deg, #449ca2 20%, #e63f33 70%);
 
-  background: -moz-linear-gradient(315deg, #449CA2 20%, #E63F33 70%);
+  background: -moz-linear-gradient(315deg, #449ca2 20%, #e63f33 70%);
 
-  background: -webkit-linear-gradient(315deg, #449CA2 20%, #E63F33 70%);
+  background: -webkit-linear-gradient(315deg, #449ca2 20%, #e63f33 70%);
 
   filter: progid: DXImageTransform.Microsoft.gradient( startColorstr="#449BA2", endColorstr="#E63D32", GradientType=1 );
 }
 
-.btn {		
-	transition: all 500ms cubic-bezier(0.77, 0, 0.175, 1);
+.btn {
+  transition: all 500ms cubic-bezier(0.77, 0, 0.175, 1);
 }
 
-.btn:before, .btn:after {
-	content: '';
-	position: absolute;	
-	transition: inherit;
-	z-index: -1;
+.btn:before,
+.btn:after {
+  content: '';
+  position: absolute;
+  transition: inherit;
+  z-index: -1;
 }
 
 .btn:hover:after {
-	background: #449CA2;
-	transition-delay: .35s;
+  background: #449ca2;
+  transition-delay: 0.35s;
 }
 
 .from-left:before,
--- src/components/ScrollToTopIcon.vue
+++ src/components/ScrollToTopIcon.vue
@@ -1,26 +1,26 @@
 <script setup>
-import { ref, onMounted, onBeforeUnmount } from "vue";
-import IcRoundArrowUpward from "~icons/ic/round-arrow-upward";
+import { ref, onMounted, onBeforeUnmount } from 'vue'
+import IcRoundArrowUpward from '~icons/ic/round-arrow-upward'
 
-const active = ref(false);
+const active = ref(false)
 
 const scrollBacktoTop = () => {
   if (window.scrollY > 100) {
-    active.value = true;
+    active.value = true
   } else {
-    active.value = false;
+    active.value = false
   }
-};
+}
 
 onMounted(() => {
-  window.addEventListener("load", scrollBacktoTop);
-  document.addEventListener("scroll", scrollBacktoTop);
-});
+  window.addEventListener('load', scrollBacktoTop)
+  document.addEventListener('scroll', scrollBacktoTop)
+})
 
 onBeforeUnmount(() => {
-  window.addEventListener("load", scrollBacktoTop);
-  document.addEventListener("scroll", scrollBacktoTop);
-});
+  window.addEventListener('load', scrollBacktoTop)
+  document.addEventListener('scroll', scrollBacktoTop)
+})
 </script>
 
 <template>
@@ -34,9 +34,9 @@ onBeforeUnmount(() => {
       force: true,
       cancelable: true,
       x: false,
-      y: true,
+      y: true
     }"
-    class="bg-dark-700 fixed bottom-5 p-3 rounded-md right-5 transition-all ease-in duration-200 hover:bg-dark-800 cursor-pointer z-50"
+    class="fixed z-50 p-3 transition-all duration-200 ease-in rounded-md cursor-pointer bg-dark-700 bottom-5 right-5 hover:bg-dark-800"
     :class="active ? 'opacity-1' : 'opacity-0'"
   >
     <IcRoundArrowUpward />
